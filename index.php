201fbb078d967af7c4b1dbb9fd8fa3def9c11206
f5rus <millionaire@mail.ru>
2014-10-08 10:53:07 +0700

    dfgdfgfd

---
+++ index.php
@@ -0,0 +1,37 @@
+﻿<?php
+    Class Magic 
+    {               
+        function __construct() {
+            echo "я создан </br>";
+        }
+        
+        function __clone()
+        {
+            echo "я клонирован</br>";
+        }
+            
+        function __get($name)
+        {
+            echo "у меня запросили название переменной</br>";
+        }
+        
+        function __call($name, $arguments)
+        {
+            echo "у меня вызвали название метода</br>";
+        }
+        
+        function __toString()
+        {		
+            return __CLASS__;
+        }
+                       
+    }
+
+    $mag = new Magic();
+    $magClon = clone $mag; 
+    $mag->per;
+    $mag->myMethod();
+    echo $mag;
+    
+    
+?>
